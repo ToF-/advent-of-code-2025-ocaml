be69f855245bc6582f95e00ee63c681cca09c219
ToF- <cthibauttof@gmail.com>
2026-04-07 17:33:07 +0200

    gift shop tools

--- lib/gift_shop.ml
+++ lib/gift_shop.ml
@@ -1,3 +1,15 @@
+let pairs_of_ints s =
+  List.map
+    (fun x -> List.map int_of_string (String.split_on_char '-' x))
+    (String.split_on_char ',' s)
+
+let rec len n = if n < 10 then 1 else 1 + len (n / 10)
+let power_of_ten n = int_of_float (10.0 ** float_of_int n)
+
+let half n =
+  let l = len n in
+  n / power_of_ten (l / 2)
+
 let is_invalid id =
   let l = String.length id in
   match l with
