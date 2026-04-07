834582dd78075028e9d9296985e9f3fef3fc1aac
ToF- <cthibauttof@gmail.com>
2026-04-07 14:04:27 +0200

    passing tests

--- test/test_advent_of_code_2025_ocaml.ml
+++ test/test_advent_of_code_2025_ocaml.ml
@@ -16,7 +16,7 @@ let tests =
              (Secret_entrance.password "../testdata/day01_sample.txt"
                 Secret_entrance.passes_on_zero) );
          ( "secret entrance b with input" >:: fun _ ->
-           assert_equal ~printer:string_of_int 964
+           assert_equal ~printer:string_of_int 5872
              (Secret_entrance.password "../testdata/day01_input.txt"
                 Secret_entrance.passes_on_zero) );
        ]
