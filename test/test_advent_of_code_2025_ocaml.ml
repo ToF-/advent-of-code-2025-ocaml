728811cd630fc128daa278a1d883e5cda0643552
ToF- <cthibauttof@gmail.com>
2026-04-07 15:03:40 +0200

    modularized tests

--- test/test_advent_of_code_2025_ocaml.ml
+++ test/test_advent_of_code_2025_ocaml.ml
@@ -1,25 +1,26 @@
 open OUnit2
 
 let suite name =
+  let password = Advent_of_code_2025.Secret_entrance.password in
+  let stops_at_zero = Advent_of_code_2025.Secret_entrance.stops_at_zero in
+  let passes_on_zero = Advent_of_code_2025.Secret_entrance.passes_on_zero in
+  let sample = "../testdata/day01_sample.txt" in
+  let input = "../testdata/day01_input.txt" in
   Printf.printf "%s\n" name;
   name
   >::: [
          ( "secret entrance a with sample" >:: fun _ ->
-           assert_equal ~printer:string_of_int 3
-             (Secret_entrance.password "../testdata/day01_sample.txt"
-                Secret_entrance.stops_at_zero) );
+           assert_equal ~printer:string_of_int 3 (password sample stops_at_zero)
+         );
          ( "secret entrance a with input" >:: fun _ ->
            assert_equal ~printer:string_of_int 964
-             (Secret_entrance.password "../testdata/day01_input.txt"
-                Secret_entrance.stops_at_zero) );
+             (password input stops_at_zero) );
          ( "secret entrance b with sample" >:: fun _ ->
            assert_equal ~printer:string_of_int 6
-             (Secret_entrance.password "../testdata/day01_sample.txt"
-                Secret_entrance.passes_on_zero) );
+             (password sample passes_on_zero) );
          ( "secret entrance b with input" >:: fun _ ->
            assert_equal ~printer:string_of_int 5872
-             (Secret_entrance.password "../testdata/day01_input.txt"
-                Secret_entrance.passes_on_zero) );
+             (password input passes_on_zero) );
        ]
 
 let _ = run_test_tt_main (suite "secret entrance")
