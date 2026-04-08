336f327fe596bd14cb5d444c17456f3e818503fd
ToF- <cthibauttof@gmail.com>
2026-04-08 14:33:28 +0200

    enumerate invalid ids accross ranges of different len

--- lib/gift_shop.ml
+++ lib/gift_shop.ml
@@ -6,17 +6,25 @@ let pairs_of_ints s =
 let rec len n = if n < 10 then 1 else 1 + len (n / 10)
 let rec pow n = match n with 0 -> 1 | m -> 10 * pow (n - 1)
 
-let invalid_ids first last =
+let rec invalid_ids first last =
   let rec invalid_ids_aux id step limit last =
-    if id > last || id > limit then [] else id :: invalid_ids_aux (id + step) step limit last
+    Printf.printf "id:%d step:%d limit:%d last:%d\n" id step limit last;
+    if id > last || id > limit then []
+    else id :: invalid_ids_aux (id + step) step limit last
   in
   let l = len first in
+  let m = len last in
   let p = pow (l / 2) in
   let a = (first / p * p) + (first / p) in
   let step = p + 1 in
-  let limit = (pow l) - 1 in
+  let limit = pow l - 1 in
   let start = if a >= first then a else a + step in
-  invalid_ids_aux start step limit last
+  if start > last then []
+  else
+    List.append
+      (if l mod 2 == 0 then invalid_ids_aux start step limit last
+       else invalid_ids (limit + 1) last)
+      (if l < m then invalid_ids (limit + 1) last else [])
 
 let sum_invalid_ids intervals =
   match intervals with
