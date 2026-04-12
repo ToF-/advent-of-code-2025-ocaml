4e2c54748f91153e7fbfd19f3e2f3dfc509f1cac
ToF- <cthibauttof@gmail.com>
2026-04-12 21:59:45 +0200

    day05 wip

--- lib/cafeteria.ml
+++ lib/cafeteria.ml
@@ -1,9 +1,14 @@
 open Utils
 
 let acquire lines =
-    let (a, b) = List.partition (fun s -> String.contains s '-') lines in
-    (List.map (fun s ->
-        let (i,jparse_interval a, List.map Int.of_string b)
+    let parse_interval s =
+        match String.split_on_char '-' s with
+        | [s1;s2] -> (int_of_string s1, int_of_string s2)
+        | _ -> invalid_arg "incorrect interval"
+    in
+    let (ss1, ss2) = List.partition (fun s -> String.contains s '-') lines in
+    (List.map (fun s -> parse_interval s) ss1,
+    List.map (fun s -> int_of_string s) ss2)
 
 let fresh_ingredients file_name =
     let lines = Utils.read_lines file_name in
