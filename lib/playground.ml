2f45b129c29783da033d5a41c5ed77b367a3ca73
ToF- <cthibauttof@gmail.com>
2026-04-16 00:28:13 +0200

    day 08 A wip

--- lib/playground.ml
+++ lib/playground.ml
@@ -23,11 +23,12 @@ let connect map a b =
   let circuit_a = map |> CircuitMap.find a in
   let circuit_b = map |> CircuitMap.find b in
   let new_circuit = circuit_a |> Circuit.union circuit_b in
-  map
-  |> CircuitMap.update b (fun opt ->
-      match opt with None -> None | Some circuit -> Some new_circuit)
-  |> CircuitMap.update a (fun opt ->
-      match opt with None -> None | Some circuit -> Some new_circuit)
+  Circuit.fold
+    (fun box acc ->
+      acc
+      |> CircuitMap.update box (fun opt ->
+          match opt with None -> None | Some circuit -> Some new_circuit))
+    new_circuit map
 
 let init_map boxes =
   List.fold_left
@@ -35,20 +36,61 @@ let init_map boxes =
       map |> CircuitMap.add box (Circuit.empty |> Circuit.add box))
     CircuitMap.empty boxes
 
-let print_box (a,b,c) = 
-    Printf.printf "(%d,%d,%d);" a b c
+let print_box (a, b, c) = Printf.printf "(%d,%d,%d);" a b c
 
-let print_circuit circuit = 
-    circuit |> Circuit.to_list |> List.iter (fun box ->
-        print_box box)
+let print_circuit circuit =
+  circuit |> Circuit.to_list |> List.iter (fun box -> print_box box)
 
+let print_circuits circuits =
+  List.iter
+    (fun c ->
+      print_circuit c;
+      Printf.printf "\n")
+    circuits
 
-let print_map map = 
-    CircuitMap.iter (fun box circuit ->
-        print_box box ;
-        Printf.printf "\n[" ;
-        print_circuit circuit ;
-        Printf.printf "]\n") map
+let print_map map =
+  CircuitMap.iter
+    (fun box circuit ->
+      if Circuit.cardinal circuit > 1 then (
+        print_box box;
+        Printf.printf "\n[";
+        print_circuit circuit;
+        Printf.printf "]\n")
+      else Printf.printf "\n")
+    map
+
+let print_product p =
+  List.iter
+    (fun (d, (px, py, pz), (qx, qy, qz)) ->
+      Printf.printf "(%f, (%d,%d,%d), (%d,%d,%d))\n" d px py pz qx qy qz)
+    p
+
+let distance (p1, p2, p3) (q1, q2, q3) =
+  let px, py, pz = (float_of_int p1, float_of_int p2, float_of_int p3) in
+  let qx, qy, qz = (float_of_int q1, float_of_int q2, float_of_int q3) in
+  let dx, dy, dz = (px -. qx, py -. qy, pz -. qz) in
+  sqrt ((dx *. dx) +. (dy *. dy) +. (dz *. dz))
+
+let rec all_product f list =
+  match list with
+  | [] -> []
+  | [ a ] -> []
+  | x :: rest ->
+      List.append (List.map (fun y -> f x y) rest) (all_product f rest)
+
+let box_distance a b = (distance a b, a, b)
+
+let connect_boxes map distances =
+  List.fold_left (fun acc (d, a, b) -> connect acc a b) map distances
+
+let rec uniq circuits =
+  match circuits with
+  | [] -> []
+  | [ a ] -> [ a ]
+  | a :: b :: rest ->
+          let cmp = compare (Circuit.to_list a) (Circuit.to_list b) in
+      if cmp == 0 then uniq (b :: rest)
+      else a :: uniq (b :: rest)
 
 let circuit_product file_name =
   let lines = Utils.read_lines file_name in
@@ -61,5 +103,19 @@ let circuit_product file_name =
       lines
   in
   let map = init_map boxes in
-  print_map map ;
-  List.length boxes
+  let distances = List.sort compare (all_product box_distance boxes) in
+  let final_map = connect_boxes map (List.take 10 distances) in
+  let circuits =
+    final_map |> CircuitMap.bindings
+    |> List.map (fun (k, v) -> v)
+    |> List.sort (fun c d -> compare (Circuit.cardinal d) (Circuit.cardinal c))
+    |> List.sort (fun c d -> compare (Circuit.to_list c) (Circuit.to_list d))
+    |> uniq
+    |> List.sort (fun c d -> compare (Circuit.cardinal d) (Circuit.cardinal c))
+  in
+  print_circuits circuits;
+  match circuits with
+  | a :: b :: c :: _ ->
+          Circuit.cardinal a * Circuit.cardinal b * Circuit.cardinal c
+  | _ ->
+    invalid_arg "not enough circuits"  
--- test/test_playground.ml
+++ test/test_playground.ml
@@ -2,10 +2,13 @@ open OUnit2
 
 let tests =
   let sample = "../testdata/day08_sample.txt" in
+  let input =  "../testdata/day08_input.txt" in
   let circuit_product = Advent_of_code_2025.Playground.circuit_product in
   Printf.printf "day 07: laboratories\n";
   "laboratories"
   >::: [
          ( "total splits for sample (A)" >:: fun _ ->
-           assert_equal ~printer:string_of_int 0 (circuit_product sample) );
+           assert_equal ~printer:string_of_int 40 (circuit_product sample) );
+         ( "total splits for input (A)" >:: fun _ ->
+           assert_equal ~printer:string_of_int 0 (circuit_product input) );
        ]
